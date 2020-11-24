336f93804b6d91acb9728f34d0ac80d099a43f4b
Kris Renaldi <krenaldi@comcast.net>
2020-11-23 19:47:44 -0500

    Added Docker to app

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
 
-// Connect to MongoDB
+// Connect to MongoDB thru mongo container
 mongoose
   .connect(
     'mongodb://mongo:27017/docker-node-mongo',
